4c1b04a64869e89dadcbad766dd91b984b01b500
choi ji won <w2019@e-mirim.hs.kr>
2021-06-03 20:52:21 +0900

    Completion ViewFlipper Commit

--- app/src/main/java/kr/hs/emirim/w2019/ex6/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/ex6/MainActivity.java
@@ -3,12 +3,38 @@ package kr.hs.emirim.w2019.ex6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    ViewFlipper flipper;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.ex6_15);
+
+        flipper = findViewById(R.id.flipper);
+        Button btnPrev = findViewById(R.id.btn_prev);
+        Button btnNext = findViewById(R.id.btn_next);
+
+        btnPrev.setOnClickListener(btnListener);
+        btnNext.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.btn_prev:
+                    flipper.showPrevious();
+                    break;
+                case R.id.btn_next:
+                    flipper.showNext();
+                    break;
+            }
+        }
+    };
 }
